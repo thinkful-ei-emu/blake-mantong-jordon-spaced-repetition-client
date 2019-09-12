657abdb63b243be1a61392cd221752b59530d2d7
Jvymvn <Jordon.carter@yahoo.com>
2019-09-12 14:45:36 -0400

    Auth

--- src/services/dashboard-api-service.js
+++ src/services/dashboard-api-service.js
@@ -3,43 +3,44 @@ import TokenService from './token-service'
 
 
 const DashboardService = {
-  getLanguageAndWords(){
+  getLanguageAndWords() {
     return fetch(`${config.API_ENDPOINT}/language`, {
       method: 'GET',
       headers: {
         'authorization': `Bearer ${TokenService.getAuthToken()}`,
       },
     })
-    .then(res => res.json())
-    .then(data => {
-      return data;
-    });
+      .then(res => res.json())
+      .then(data => {
+        return data;
+      });
   },
-  getHead(){
+  getHead() {
     return fetch(`${config.API_ENDPOINT}/language/head`, {
       method: 'GET',
       headers: {
         'authorization': `Bearer ${TokenService.getAuthToken()}`,
       },
     })
-    .then(res => res.json())
-    .then(data => {
-      return data;
-    });
+      .then(res => res.json())
+      .then(data => {
+        return data;
+      });
   },
-  postGuess( answer = {} ){
-   return fetch(`${config.API_ENDPOINT}/language/guess`, {
-     method: 'POST',
-     headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify( answer ),
-  })
-    .then(res =>
-      (!res.ok)
-        ? res.json().then(err => Promise.reject(err))
-        : res.json()
-    )
- }
+  postGuess(answer = {}) {
+    return fetch(`${config.API_ENDPOINT}/language/guess`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `Bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify(answer),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(err => Promise.reject(err))
+          : res.json()
+      )
+  }
 }
 export default DashboardService;
